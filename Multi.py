e990b958ec66b50c212817d6f8fe03a65d356a0d
Payalk0601 <payalkakade0601@gmail.com>
2025-02-28 15:32:59 +0530

    Update and rename Multiplication.py to Multi.py

--- Multi.py
+++ Multi.py
@@ -5,6 +5,6 @@ num2 = float(input("Enter second number: "))
 # Multiplying the numbers
 product = num1 * num2
 
-# Displaying the result
-print("The sum  is:", product)
+# Displaying the result with the correct label
+print("The product is:", product)
 
